f4fb4252f2bc06b7be577c686b22bb1cf38b4bf1
Stefan Kuethe <stefan.kuethe@eoda.de>
2025-01-21 14:33:46 +0100

    First typescript py version

---
+++ py-bindings/test.js
@@ -0,0 +1,27 @@
+// python3 -m http.server
+
+"use strict";
+
+const CARTO_THEME = "dark-matter";
+
+console.log("test");
+
+const layer = {
+
+};
+
+const mapData = {
+    mapOptions: {
+        style: `https://basemaps.cartocdn.com/gl/${CARTO_THEME}-gl-style/style.json`,
+        center: [-87.61694, 41.86625],
+        zoom: 17,
+        pitch: 40,
+        bearing: 20,
+        antialias: true,
+    },
+    calls: [
+        ["flyTo", [{ "center": [-87.61694, 41.7] }]]
+    ]
+};
+
+pymaplibregl(mapData);
---
+++ src-ts/types.ts
@@ -0,0 +1,23 @@
+const maplibregl = (window as any).maplibregl;
+const deck = (window as any).deck;
+const MapboxDraw = (window as any).MapboxDraw;
+const Shiny = (window as any).Shiny;
+const HTMLWidgets = (window as any).HTMLWidgets;
+
+type MapOptions = {
+  container: string;
+};
+
+type MapData = {
+  mapOptions: MapOptions;
+  calls: [string, any][];
+};
+
+type Payload = {
+  mapData: MapData;
+};
+
+type Popup = {
+  text: string;
+  options: object;
+};
